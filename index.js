ec7af2be8d2cb7e754bfcd0867d46df2a1a6e80f
Your Name <abbasovelshen@mail.ru>
2023-02-14 15:23:52 +0400

    scroll

--- index.js
+++ index.js
@@ -171,6 +171,21 @@ lGBP.addEventListener("mouseup",()=>{
   leftpar.innerHTML=`1 ${value2} = ${obj.rates.GBP} ${value1}`
     })
 })
+var manubtn = document.getElementById("manubtn")
+var sidenav = document.getElementById("sidenav")
+var menu = document.getElementById("menu")
 
+sidenav.style.right = "-250px"
+
+manubtn.onclick = function () {
+    if (sidenav.style.right == "-250px") {
+        sidenav.style.right = "0";
+        menu.src = "./img/close.png";
+    }
+    else {
+        sidenav.style.right = "-250px";
+        menu.src = "./img/222.png";
+    }
+}
 
 
